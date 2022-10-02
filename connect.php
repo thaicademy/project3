bfef41f53f33c30a607ba6b20f5a2640960a5e85
siamcodes <somkiat@gmail.com>
2022-10-02 16:03:21 +0700

    first commit

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "root1234";
+$dbname = "db_sunday";  //ชื่อฐานข้อมูล
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Check connection
+if (!$conn) {
+  die("ผิดพลาด: " . mysqli_connect_error());
+}
+
+//echo "เชื่อมต่อฐานข้อมูลได้";
+?>
